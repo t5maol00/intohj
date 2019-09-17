6360c407ced8450cc98df9136f43dcbf469744bc
Olli Märsy <t5maol00@students.oamk.fi>
2019-09-17 19:26:34 +0300

    basic php muokattu

--- basic.php
+++ basic.php
@@ -3,4 +3,37 @@
   <p>
     This is the PHP Basic.
   </p>
+  <h2>Variables</h2>
+  <p>
+    Muuttujien nimet alkavat aina dollarimerkillä.
+  </p>
+  <?php
+    $name="Olli";
+    echo 'Terve '.$name;
+  ?>
+
+  <h2>if-else</h2>
+
+  <?php
+    $year=2019;
+    if($year < 2000){
+      echo 'Ennen vuotta 2000';
+    }
+    else {
+      echo "Vuoden 2000 jälkeen";
+    }
+  ?>
+
+  <h2>Loopit</h2>
+  <p>
+    PHP:ssä voi käyttää for, while ja foreach looppeja.
+  </p>
+
+  <?php
+  for($x=1; $x <= 5; $x++){
+      echo 'Rivi '.$x.'<br>';
+  }
+  ?>
+
+
 <?php include "footer.php"; ?>
