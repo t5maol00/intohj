12d36cfbc2a68b86d589ef39b1bb182f3ed11c18
Olli Märsy <t5maol00@students.oamk.fi>
2019-10-06 19:15:42 +0300

    ex5 tehty

--- get.php
+++ get.php
@@ -1,6 +1,14 @@
 <?php include "menu.php"; ?>
-  <h2>Get</h2>
+  <h2>GET</h2>
   <p>
     This is the Get.
+    <form class="" action="show_get.php" method="get">
+      <label for="">Etunimi</label> <br>
+      <input type="text" name="en" value="" placeholder="etunimi"> <br><br>
+      <label for="">Sukunimi</label> <br>
+      <input type="text" name="sn" value="" placeholder="sukunimi"> <br>
+      <br>
+      <input type="submit" name="" value="Save">
+    </form>
   </p>
 <?php include "footer.php"; ?>
